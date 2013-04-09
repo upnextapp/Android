1935475f27c005fd656df2f4e189e50b94fa5df7
Si Wan Kim <siwan.kim@utexas.edu>
2013-04-08 20:53:11 -0500

    committing initial page change

--- gen/com/stg/inqueue/R.java
+++ gen/com/stg/inqueue/R.java
@@ -14,21 +14,23 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int cancel=0x7f07000a;
         public static final int firstName=0x7f070003;
         public static final int initial_row0=0x7f070001;
         public static final int initial_row1=0x7f070002;
         public static final int initial_row3=0x7f070008;
         public static final int intial_row2=0x7f070005;
         public static final int lastName=0x7f070006;
-        public static final int menu_about=0x7f07000d;
-        public static final int menu_help=0x7f07000e;
-        public static final int menu_settings=0x7f07000c;
-        public static final int tab1=0x7f07000a;
-        public static final int tab2=0x7f07000b;
+        public static final int menu_about=0x7f07000f;
+        public static final int menu_help=0x7f070010;
+        public static final int menu_settings=0x7f07000e;
+        public static final int submit=0x7f070009;
+        public static final int tab1=0x7f07000c;
+        public static final int tab2=0x7f07000d;
         public static final int tableLayout=0x7f070000;
-        public static final int textView1=0x7f070009;
-        public static final int userFirstName=0x7f070004;
-        public static final int userLastName=0x7f070007;
+        public static final int textView1=0x7f07000b;
+        public static final int userEMail=0x7f070004;
+        public static final int userPassword=0x7f070007;
     }
     public static final class layout {
         public static final int intialpage=0x7f030000;
@@ -42,13 +44,15 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int firstName=0x7f040005;
+        public static final int cancel=0x7f04000b;
+        public static final int e_mail=0x7f040005;
         public static final int hello_world=0x7f040001;
-        public static final int lastName=0x7f040006;
         public static final int menu_about=0x7f040003;
         public static final int menu_help=0x7f040004;
         public static final int menu_settings=0x7f040002;
+        public static final int password=0x7f040006;
         public static final int splash=0x7f040009;
+        public static final int submit=0x7f04000a;
         public static final int title_activity_splash_=0x7f040008;
         public static final int welcome=0x7f040007;
     }
