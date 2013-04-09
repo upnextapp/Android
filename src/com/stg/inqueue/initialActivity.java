68f47e8fc9194a5abd05aa216486710f3373cf01
Si Wan Kim <siwan.kim@utexas.edu>
2013-04-08 23:18:17 -0500

    JSON & HTTP Post Request intial setup

--- src/com/stg/inqueue/initialActivity.java
+++ src/com/stg/inqueue/initialActivity.java
@@ -1,7 +1,17 @@
 package com.stg.inqueue;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -14,7 +24,7 @@ public class initialActivity extends Activity {
 	private EditText password;
 	private Button submitButton;
 	private Button cancelButton;
-
+	private JSONObject jObject;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -41,8 +51,23 @@ public class initialActivity extends Activity {
 	public OnClickListener sumbitButtonListener = new OnClickListener(){
 
 		@Override
-		public void onClick(View v) {
-			// TODO Auto-generated method stub
+		public void onClick(View v){
+			
+			String enteredEmail = email.getText().toString();
+			String enteredPassword = password.getText().toString();
+			
+			try {
+				parseJSON(enteredEmail, enteredPassword);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			if(jObject != null){
+				sendHTTPPost(jObject);
+			}else{
+				Log.d("JSON object is not parsed", "error JSON");
+			}
 			
 		}
 	};
@@ -55,5 +80,28 @@ public class initialActivity extends Activity {
 			password.setText(R.string.empty);
 		}
 	};
+	
+	public void parseJSON(String em, String pw) throws Exception{
+		jObject.put("Email", em);
+		jObject.put("Password", pw);
+	}
+	
+	public void sendHTTPPost(JSONObject jObject){
+		String url = ""; //need our server page
+		HttpClient httpClient = new DefaultHttpClient();
+		try{
+			HttpPost request = new HttpPost(url);
+			StringEntity params =new StringEntity(jObject.toString());
+			request.addHeader("content-type", "application/x-www-form-urlencoded");
+	        request.setEntity(params);
+	        HttpResponse response = httpClient.execute(request);
+		}
+		catch(Exception e){
+			
+		}
+		finally{
+			httpClient.getConnectionManager().shutdown();
+		}
+	}
 
 }
