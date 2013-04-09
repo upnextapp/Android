1e83432a67d4225628d41f6b02449af9b250d125
Thinh Lam <thinh.lam@utexas.edu>
2013-04-08 23:49:36 -0500

    Add two tabs.

--- src/com/stg/inqueue/MainActivity.java
+++ src/com/stg/inqueue/MainActivity.java
@@ -1,125 +1,182 @@
 package com.stg.inqueue;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
-import android.annotation.TargetApi;
 import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.ActionBar.TabListener;
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
-
+	public RestaurantsFragment listRestaurantsFragment;
+	public PositionFragment positionFragment;
+	public ArrayList<String> restaurantsArrayList;
 
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-//        ActionBar actionBar = getActionBar();
-//        actionBar.hide();
-        
-        //testing purpose
-        //Toast.makeText(this, getDeviceID(), Toast.LENGTH_LONG).show();
-        //Toast.makeText(this, getPhoneNumber(), Toast.LENGTH_LONG).show();
-    }
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
-    
-    public String getDeviceID() {
-    	/*
-    	//String Return_DeviceID = USERNAME_and_PASSWORD.getString(DeviceID_key,"Guest");
-    	//return Return_DeviceID;
+//		listRestaurantsFragment = (RestaurantsFragment) getFragmentManager()
+//				.findFragmentById(R.id.restaurants);
+//		positionFragment = new PositionFragment();
+		
+		setupTabs();
+		// ActionBar actionBar = getActionBar();
+		// actionBar.hide();
 
-    	TelephonyManager TelephonyMgr = (TelephonyManager) getApplicationContext().getApplicationContext().getSystemService(Context.TELEPHONY_SERVICE);
-    	String m_szImei = TelephonyMgr.getDeviceId(); // Requires
-    	// READ_PHONE_STATE
+		// testing purpose
+		// Toast.makeText(this, getDeviceID(), Toast.LENGTH_LONG).show();
+		// Toast.makeText(this, getPhoneNumber(), Toast.LENGTH_LONG).show();
+	}
 
-    	// 2 compute DEVICE ID
-    	String m_szDevIDShort = "35"
-    	+ // we make this look like a valid IMEI
-    	Build.BOARD.length() % 10 + Build.BRAND.length() % 10
-    	+ Build.CPU_ABI.length() % 10 + Build.DEVICE.length() % 10
-    	+ Build.DISPLAY.length() % 10 + Build.HOST.length() % 10
-    	+ Build.ID.length() % 10 + Build.MANUFACTURER.length() % 10
-    	+ Build.MODEL.length() % 10 + Build.PRODUCT.length() % 10
-    	+ Build.TAGS.length() % 10 + Build.TYPE.length() % 10
-    	+ Build.USER.length() % 10; // 13 digits
-    	// 3 android ID - unreliable
-    	String m_szAndroidID = Secure.getString(getContentResolver(),Secure.ANDROID_ID);
-    	// 4 wifi manager, read MAC address - requires
-    	// android.permission.ACCESS_WIFI_STATE or comes as null
-    	WifiManager wm = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-    	String m_szWLANMAC = wm.getConnectionInfo().getMacAddress();
-    	// 5 Bluetooth MAC address android.permission.BLUETOOTH required
-    	BluetoothAdapter m_BluetoothAdapter = null; // Local Bluetooth adapter
-    	m_BluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-    	String m_szBTMAC = m_BluetoothAdapter.getAddress();
-    	System.out.println("m_szBTMAC "+m_szBTMAC);
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    	// 6 SUM THE IDs
-    	String m_szLongID = m_szImei + m_szDevIDShort + m_szAndroidID+ m_szWLANMAC + m_szBTMAC;
-    	System.out.println("m_szLongID "+m_szLongID);
-    	MessageDigest m = null;
-    	try {
-    	m = MessageDigest.getInstance("MD5");
-    	} catch (NoSuchAlgorithmException e) {
-    	e.printStackTrace();
-    	                }
-    	m.update(m_szLongID.getBytes(), 0, m_szLongID.length());
-    	byte p_md5Data[] = m.digest();
+	public String getDeviceID() {
+		/*
+		 * //String Return_DeviceID =
+		 * USERNAME_and_PASSWORD.getString(DeviceID_key,"Guest"); //return
+		 * Return_DeviceID;
+		 * 
+		 * TelephonyManager TelephonyMgr = (TelephonyManager)
+		 * getApplicationContext
+		 * ().getApplicationContext().getSystemService(Context
+		 * .TELEPHONY_SERVICE); String m_szImei = TelephonyMgr.getDeviceId(); //
+		 * Requires // READ_PHONE_STATE
+		 * 
+		 * // 2 compute DEVICE ID String m_szDevIDShort = "35" + // we make this
+		 * look like a valid IMEI Build.BOARD.length() % 10 +
+		 * Build.BRAND.length() % 10 + Build.CPU_ABI.length() % 10 +
+		 * Build.DEVICE.length() % 10 + Build.DISPLAY.length() % 10 +
+		 * Build.HOST.length() % 10 + Build.ID.length() % 10 +
+		 * Build.MANUFACTURER.length() % 10 + Build.MODEL.length() % 10 +
+		 * Build.PRODUCT.length() % 10 + Build.TAGS.length() % 10 +
+		 * Build.TYPE.length() % 10 + Build.USER.length() % 10; // 13 digits //
+		 * 3 android ID - unreliable String m_szAndroidID =
+		 * Secure.getString(getContentResolver(),Secure.ANDROID_ID); // 4 wifi
+		 * manager, read MAC address - requires //
+		 * android.permission.ACCESS_WIFI_STATE or comes as null WifiManager wm
+		 * = (WifiManager)
+		 * getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+		 * String m_szWLANMAC = wm.getConnectionInfo().getMacAddress(); // 5
+		 * Bluetooth MAC address android.permission.BLUETOOTH required
+		 * BluetoothAdapter m_BluetoothAdapter = null; // Local Bluetooth
+		 * adapter m_BluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		 * String m_szBTMAC = m_BluetoothAdapter.getAddress();
+		 * System.out.println("m_szBTMAC "+m_szBTMAC);
+		 * 
+		 * // 6 SUM THE IDs String m_szLongID = m_szImei + m_szDevIDShort +
+		 * m_szAndroidID+ m_szWLANMAC + m_szBTMAC;
+		 * System.out.println("m_szLongID "+m_szLongID); MessageDigest m = null;
+		 * try { m = MessageDigest.getInstance("MD5"); } catch
+		 * (NoSuchAlgorithmException e) { e.printStackTrace(); }
+		 * m.update(m_szLongID.getBytes(), 0, m_szLongID.length()); byte
+		 * p_md5Data[] = m.digest();
+		 * 
+		 * String m_szUniqueID = new String(); for (int i = 0; i <
+		 * p_md5Data.length; i++) { int b = (0xFF & p_md5Data[i]); // if it is a
+		 * single digit, make sure it have 0 in front (proper // padding) if (b
+		 * <= 0xF) m_szUniqueID += "0"; // add number to string m_szUniqueID +=
+		 * Integer.toHexString(b); } m_szUniqueID = m_szUniqueID.toUpperCase();
+		 * 
+		 * Log.i("-------------DeviceID------------", m_szUniqueID);
+		 * Log.d("DeviceIdCheck",
+		 * "DeviceId that generated MPreferenceActivity:"+m_szUniqueID);
+		 * 
+		 * 
+		 * //System.out.println(m_szUniqueID); return m_szUniqueID;
+		 * 
+		 * }
+		 */
 
-    	String m_szUniqueID = new String();
-    	for (int i = 0; i < p_md5Data.length; i++) {
-    	int b = (0xFF & p_md5Data[i]);
-    	// if it is a single digit, make sure it have 0 in front (proper
-    	// padding)
-    	if (b <= 0xF)
-    	m_szUniqueID += "0";
-    	// add number to string
-    	m_szUniqueID += Integer.toHexString(b);
-    	}
-    	m_szUniqueID = m_szUniqueID.toUpperCase();
+		// for prototype purposes, this is fine
+		final TelephonyManager tm = (TelephonyManager) getBaseContext()
+				.getSystemService(Context.TELEPHONY_SERVICE);
+		final String tmDevice, tmSerial, androidId;
+		tmDevice = "" + tm.getDeviceId();
+		tmSerial = "" + tm.getSimSerialNumber();
+		androidId = ""
+				+ android.provider.Settings.Secure.getString(
+						getContentResolver(),
+						android.provider.Settings.Secure.ANDROID_ID);
 
-    	Log.i("-------------DeviceID------------", m_szUniqueID);
-    	Log.d("DeviceIdCheck", "DeviceId that generated MPreferenceActivity:"+m_szUniqueID);
-    	
-    	
-    	//System.out.println(m_szUniqueID);
-    	return m_szUniqueID;
+		UUID deviceUuid = new UUID(androidId.hashCode(),
+				((long) tmDevice.hashCode() << 32) | tmSerial.hashCode());
+		String deviceId = deviceUuid.toString();
 
-    	}
-    	*/
-    
-    //for prototype purposes, this is fine
-    final TelephonyManager tm = (TelephonyManager) getBaseContext().getSystemService(Context.TELEPHONY_SERVICE);
-    final String tmDevice, tmSerial, androidId;
-    tmDevice = "" + tm.getDeviceId();
-    tmSerial = "" + tm.getSimSerialNumber();
-    androidId = "" + android.provider.Settings.Secure.getString(getContentResolver(), android.provider.Settings.Secure.ANDROID_ID);
+		return deviceId;
 
-    UUID deviceUuid = new UUID(androidId.hashCode(), ((long)tmDevice.hashCode() << 32) | tmSerial.hashCode());
-    String deviceId = deviceUuid.toString();
-    
-    return deviceId;
-    
-    }
-    
-    public String getPhoneNumber(){
-    	
-    	final TelephonyManager tm = (TelephonyManager) getBaseContext().getSystemService(Context.TELEPHONY_SERVICE);
-    	final String tmNumber;
-    	
-    	tmNumber = "" + tm.getLine1Number();
-    	return tmNumber.toString();
-    	
-    }
+	}
+
+	public String getPhoneNumber() {
+
+		final TelephonyManager tm = (TelephonyManager) getBaseContext()
+				.getSystemService(Context.TELEPHONY_SERVICE);
+		final String tmNumber;
+
+		tmNumber = "" + tm.getLine1Number();
+		return tmNumber.toString();
+
+	}
+
+	// set up the ActionBar's tabs
+	private void setupTabs() {
+		ActionBar queueActionBar = getActionBar(); // get the ActionBar
+
+		// set ActionBar's navigation mode to use tabs
+		queueActionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		// create the "Current Conditions" Tab
+		Tab currentConditionsTab = queueActionBar.newTab();
+
+		// set the Tab's title
+		currentConditionsTab.setText("Restaurants");
+
+		// set the Tab's listener
+		currentConditionsTab.setTabListener(weatherTabListener);
+		queueActionBar.addTab(currentConditionsTab); // add the Tab
+
+		// create the "Five Day Forecast" tab
+		Tab fiveDayForecastTab = queueActionBar.newTab();
+		fiveDayForecastTab.setText("Position");
+		fiveDayForecastTab.setTabListener(weatherTabListener);
+		queueActionBar.addTab(fiveDayForecastTab);
+
+		// select "Current Conditions" Tab by default
+	} // end method setupTabs
+
+	// listen for events generated by the ActionBar Tabs
+	TabListener weatherTabListener = new TabListener() {
+		// called when the selected Tab is re-selected
+		@Override
+		public void onTabReselected(Tab arg0, FragmentTransaction arg1) {
+		} // end method onTabReselected
+
+		// called when a previously unselected Tab is selected
+		@Override
+		public void onTabSelected(Tab tab, FragmentTransaction arg1) {
+			// display the information corresponding to the selected Tab
+			if (tab.getPosition() == 1)
+				setContentView(R.layout.position_in_line);
+			else
+				setContentView(R.layout.list_of_restaurants);
+		} // end method onTabSelected
+
+		// called when a tab is unselected
+		@Override
+		public void onTabUnselected(Tab arg0, FragmentTransaction arg1) {
+		} // end method onTabSelected
+	}; // end WeatherTabListener
 }
