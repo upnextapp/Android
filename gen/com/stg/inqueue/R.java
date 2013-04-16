1e709f5ccdb5fb5765f0e8b2fa53622837a43d92
Thinh Lam <thinh.lam@utexas.edu>
2013-04-15 23:41:58 -0500

    Revamp UI. Need to add new fragment to display line position.

--- gen/com/stg/inqueue/R.java
+++ gen/com/stg/inqueue/R.java
@@ -10,55 +10,66 @@ package com.stg.inqueue;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        /**  Default screen margins, per the Android Design guidelines. 
+         */
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int cancel=0x7f07000a;
-        public static final int firstName=0x7f070003;
-        public static final int initial_row0=0x7f070001;
-        public static final int initial_row1=0x7f070002;
-        public static final int initial_row3=0x7f070008;
-        public static final int intial_row2=0x7f070005;
-        public static final int lastName=0x7f070006;
-        public static final int menu_about=0x7f070010;
-        public static final int menu_help=0x7f070011;
-        public static final int menu_settings=0x7f07000f;
-        public static final int position=0x7f07000d;
-        public static final int restaurant_position_title=0x7f07000c;
-        public static final int restaurants_available_list=0x7f07000b;
-        public static final int submit=0x7f070009;
-        public static final int tableLayout=0x7f070000;
-        public static final int textView1=0x7f07000e;
-        public static final int userEMail=0x7f070004;
-        public static final int userPassword=0x7f070007;
+        public static final int cancel=0x7f08000a;
+        public static final int firstName=0x7f080003;
+        public static final int initial_row0=0x7f080001;
+        public static final int initial_row1=0x7f080002;
+        public static final int initial_row3=0x7f080008;
+        public static final int intial_row2=0x7f080005;
+        public static final int lastName=0x7f080006;
+        public static final int menu_about=0x7f080011;
+        public static final int menu_help=0x7f080012;
+        public static final int menu_settings=0x7f080010;
+        public static final int pager=0x7f08000b;
+        public static final int pager_title_strip=0x7f08000c;
+        public static final int position=0x7f08000e;
+        public static final int restaurant_position_title=0x7f08000d;
+        public static final int submit=0x7f080009;
+        public static final int tableLayout=0x7f080000;
+        public static final int textView1=0x7f08000f;
+        public static final int userEMail=0x7f080004;
+        public static final int userPassword=0x7f080007;
     }
     public static final class layout {
-        public static final int intialpage=0x7f030000;
-        public static final int main=0x7f030001;
-        public static final int position_in_line=0x7f030002;
-        public static final int restaurant_row=0x7f030003;
-        public static final int welcome=0x7f030004;
+        public static final int fragment_main_dummy=0x7f030000;
+        public static final int intialpage=0x7f030001;
+        public static final int main=0x7f030002;
+        public static final int position_in_line=0x7f030003;
+        public static final int restaurant_row=0x7f030004;
+        public static final int welcome=0x7f030005;
     }
     public static final class menu {
-        public static final int main=0x7f060000;
-        public static final int splash_layout=0x7f060001;
+        public static final int main=0x7f070000;
+        public static final int splash_layout=0x7f070001;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int cancel=0x7f04000b;
-        public static final int e_mail=0x7f040005;
-        public static final int empty=0x7f04000c;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_about=0x7f040003;
-        public static final int menu_help=0x7f040004;
-        public static final int menu_settings=0x7f040002;
-        public static final int password=0x7f040006;
-        public static final int position=0x7f04000d;
-        public static final int splash=0x7f040009;
-        public static final int submit=0x7f04000a;
-        public static final int title_activity_splash_=0x7f040008;
-        public static final int welcome=0x7f040007;
+        public static final int app_name=0x7f050000;
+        public static final int cancel=0x7f05000b;
+        public static final int e_mail=0x7f050005;
+        public static final int empty=0x7f05000c;
+        public static final int hello_world=0x7f050001;
+        public static final int menu_about=0x7f050003;
+        public static final int menu_help=0x7f050004;
+        public static final int menu_settings=0x7f050002;
+        public static final int password=0x7f050006;
+        public static final int position=0x7f05000d;
+        public static final int splash=0x7f050009;
+        public static final int submit=0x7f05000a;
+        public static final int title_activity_splash_=0x7f050008;
+        public static final int title_section1=0x7f05000e;
+        public static final int title_section2=0x7f05000f;
+        public static final int title_section3=0x7f050010;
+        public static final int welcome=0x7f050007;
     }
     public static final class style {
         /** 
@@ -82,10 +93,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
